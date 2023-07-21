558b864f055c325a0f7da1a570a48c3e0da909c1
Aytunç Demir <112761562+aytuncdemr@users.noreply.github.com>
2023-07-21 18:59:54 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -29,7 +29,7 @@ document.querySelectorAll(".mobile-nav a").forEach((elem) => {
     });
 });
 
-document.querySelectorAll(".project")").forEach((elem) => {
+document.querySelectorAll(".project").forEach((elem) => {
     elem.addEventListener("click", function () {
         const link = this.querySelector("a").href;
         window.open(link);
